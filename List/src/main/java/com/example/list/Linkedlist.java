d5c2bcb744a0b5826355913f68a6a3497142d2ae
komal <komal.choudhary@cloudsmartz.net>
2026-02-11 11:27:55 +0530

    commit first

---
+++ List/src/main/java/com/example/list/Linkedlist.java
@@ -0,0 +1,24 @@
+package com.example.list;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Linkedlist {
+
+	public static void main(String[] args) {
+		List<Integer> numbers =new LinkedList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		System.out.println("numbers "+ numbers);
+		
+int index = numbers.indexOf(3);
+numbers.remove(index);
+//numbers.forEach(System.out::println);
+  for(Integer n: numbers) {
+	  System.out.println(n);
+  }
+	  
+	}
+
+}
---
+++ List/src/main/java/com/example/list/arraylist/Arraylist.java
@@ -0,0 +1,23 @@
+package com.example.list.arraylist;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		List<String> fruits = new ArrayList<>();
+		 fruits.add("apple");
+		 fruits.add("banana");
+		 fruits.add("watermelon");
+		  System.out.println( "fruits" + fruits);
+		    String getFruitbyindex =  fruits.get(2);
+		    System.out.println( "getfruits :" + getFruitbyindex);
+		    
+		  fruits.remove("banana");
+		  
+		  fruits.add(2,"pear");
+		  System.out.println(" get fruits after adding pear"+ fruits);
+
+	}
+
+}
